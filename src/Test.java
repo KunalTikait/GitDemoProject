28843b3cd022cbdd81583f997feab0a6d4b7a7cb
KunalTikait <kunaltikait1995@gmail.com>
2022-05-06 12:15:43 +0530

    First Commit...

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+public static void main(String[] args) {
+	int a = 10;
+	
+	if(a==10) {
+		System.out.println("Valid");
+	}
+	else {
+		System.out.println("Invalid");
+	}
+}
+}
